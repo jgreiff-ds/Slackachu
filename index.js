e381ecc1d3008189c74b018c8cf45b2ba090e51f
jgreiff-ds <jgreiff@dancorsolutions.com>
2023-02-17 13:12:42 -0500

    hope it works

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ bot.on('message', (data) => {
             return
         }
     }
-    console.log(data.username)
-    if(data.username === 'Slackachu'){
+    console.log(data.message.username)
+    if(data.message.username === 'Slackachu'){
         return
     }
     
